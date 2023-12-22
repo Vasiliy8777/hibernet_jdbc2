280884286b34f4131bf1dadc068660ca4eed367b
exs777 <exs777@mail.ru>
2023-12-22 23:24:26 +0300

    Решение через hibernate

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,25 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
+
+import java.sql.*;
+
+
 public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
-    }
+public static void main(String[] args) {
+    UserService userServ = new UserServiceImpl();
+    userServ.createUsersTable();
+    userServ.saveUser("Tom", "Doe", (byte) 23);
+    userServ.saveUser("Mot", "Fot", (byte) 53);
+    userServ.saveUser("Fred", "Dost", (byte) 93);
+    userServ.saveUser("Bob", "Ford", (byte) 45);
+    System.out.println(userServ.getAllUsers());
+    userServ.cleanUsersTable();
+    userServ.dropUsersTable();
+}
 }
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,31 +1,59 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.DatabaseMetaData;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    public void createUsersTable() {
 
-    }
+public void createUsersTable() {
+//    UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+//    userDaoJDBC.createUsersTable();
+    UserDaoHibernateImpl userDaoHibernate = new UserDaoHibernateImpl();
+    userDaoHibernate.createUsersTable();
+    // System.out.println(String.format("Таблица создана"));
+}
 
-    public void dropUsersTable() {
+public void dropUsersTable() {
+//    UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+//    userDaoJDBC.dropUsersTable();
+    UserDaoHibernateImpl userDaoHibernate = new UserDaoHibernateImpl();
+    userDaoHibernate.dropUsersTable();
+    // System.out.println(String.format("Таблица удалена"));
+}
 
-    }
+public void saveUser(String name, String lastName, byte age) {
+//        UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+//        userDaoJDBC.saveUser(name, lastName, age);
+    UserDaoHibernateImpl userDaoHibernate = new UserDaoHibernateImpl();
+    userDaoHibernate.saveUser(name, lastName, age);
+    System.out.println(String.format("User с именем - %s добавлен в базу данных", name));
+}
 
-    public void saveUser(String name, String lastName, byte age) {
+public void removeUserById(long id) {
+//        UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+//        userDaoJDBC.removeUserById(id);
+    UserDaoHibernateImpl userDaoHibernate = new UserDaoHibernateImpl();
+    userDaoHibernate.removeUserById(id);
+}
 
-    }
+public List<User> getAllUsers() {
+//        UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+//        return userDaoJDBC.getAllUsers();
+    UserDaoHibernateImpl userDaoHibernate = new UserDaoHibernateImpl();
+    return userDaoHibernate.getAllUsers();
+}
 
-    public void removeUserById(long id) {
-
-    }
-
-    public List<User> getAllUsers() {
-        return null;
-    }
-
-    public void cleanUsersTable() {
-
-    }
+public void cleanUsersTable() {
+//        UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+//        userDaoJDBC.cleanUsersTable();
+    UserDaoHibernateImpl userDaoHibernate = new UserDaoHibernateImpl();
+    userDaoHibernate.cleanUsersTable();
+}
 }
