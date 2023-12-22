f77394944fdc96a9375b5966e08b3e32adc84e6e
exs777 <exs777@mail.ru>
2023-12-23 01:32:24 +0300

    commit1

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -40,33 +40,22 @@ public void close() throws Exception {
 }
 
 //Hibernate
-
 public static SessionFactory getSessionFactory() {
     if (sessionFactory == null) {
         try {
             Configuration configuration = new Configuration();
-
-            // Hibernate settings equivalent to hibernate.cfg.xml's properties
             Properties settings = new Properties();
             settings.put(Environment.DRIVER, DRIVER);
             settings.put(Environment.URL, URL);
             settings.put(Environment.USER, USERNAME);
             settings.put(Environment.PASS, PASSWORD);
             settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
-
             settings.put(Environment.SHOW_SQL, "true");
-
             settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
-
-            // settings.put(Environment.HBM2DDL_AUTO, "create-drop");
-
             configuration.setProperties(settings);
-
             configuration.addAnnotatedClass(User.class);
-
             ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
                     .applySettings(configuration.getProperties()).build();
-
             sessionFactory = configuration.buildSessionFactory(serviceRegistry);
         } catch (Exception e) {
             e.printStackTrace();
@@ -74,6 +63,5 @@ public static SessionFactory getSessionFactory() {
     }
     return sessionFactory;
 }
-
 }
 
